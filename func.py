1c482a81dcff39208fd5354dda3c7f621ac58134
Kubalov Yann <yann40@yandex.ru>
2022-05-11 11:09:05 +0300

    update after check

--- func.py
+++ func.py
@@ -4,23 +4,23 @@ import numpy as np
 from PIL import Image
 
 
-def matrx_to_vec(frame):
+def matrx_to_vec(matrx):
     vector = []
-    for i in range(frame.shape[0]):
-        for j in range(frame.shape[1]):
-            vector.append(frame.at[i, j])
+    for i in range(matrx.shape[0]):
+        for j in range(matrx.shape[1]):
+            vector.append(matrx.at[i, j])
     return vector
 
 
-def vector_to_matrx(vec, shape):
-    matrx = np.zeros((int(len(vec) / shape), shape))
+def vector_to_matrx(vector, size):
+    matrx = np.zeros((int(len(vector) / size), size))
     for i in range(matrx.shape[0]):
         for j in range(matrx.shape[1]):
-            matrx[i, j] = vec[i * shape + j]
+            matrx[i, j] = vector[i * size + j]
     return matrx
 
 
-def matrx_to_PNG(data, file="neu.png"):
+def matrx_to_PNG(data, file="hopfield.png"):
     y = np.zeros(data.shape, dtype=np.uint8)
     y[data == 1] = 255
     y[data == -1] = 0
@@ -30,107 +30,109 @@ def matrx_to_PNG(data, file="neu.png"):
     return img
 
 
-def cal_of_neu_hopfield(axons_prev, weights):
-    axons_new = []
-    for j in range(len(axons_prev)):
+def get_hopfield(prev_ax, weights):
+    new_ax = []
+    for j in range(len(prev_ax)):
         val = 0
-        for i in range(len(axons_prev)):
-            val += weights[i, j] * axons_prev[i]
+        for i in range(len(prev_ax)):
+            val += weights[i, j] * prev_ax[i]
         if val < 0:
-            axons_new.append(-1)
+            new_ax.append(-1)
         else:
-            axons_new.append(1)
-    return axons_new
+            new_ax.append(1)
+    return new_ax
 
 
-def cal_of_neu_hamming(axons_prev, weights, t=0):
-    axons_new = []
+def get_hamming(prev_ax, weights, t=0):
+    new_ax = []
     for j in range(weights.shape[1]):
         val = 0
-        for i in range(len(axons_prev)):
-            val += weights[i, j] * axons_prev[i]
+        for i in range(len(prev_ax)):
+            val += weights[i, j] * prev_ax[i]
         val += t
-        axons_new.append(val)
-    return axons_new
+        new_ax.append(val)
+    return new_ax
 
 
-def cal_of_ax_hamming(axons_prev, eps=0.1):
-    axons_new = []
+def get_new_hamming(prev_ax, eps=0.1):
+    new_ax = []
     for j in range(4):
-        val = axons_prev[j]
+        val = prev_ax[j]
         deduct = 0
-        for i in range(len(axons_prev)):
+        for i in range(len(prev_ax)):
             if i != j:
-                deduct += axons_prev[i]
+                deduct += prev_ax[i]
         val = val - eps * deduct
         if val >= 0:
-            axons_new.append(val)
+            new_ax.append(val)
         else:
-            axons_new.append(0)
-    return axons_new
+            new_ax.append(0)
+    return new_ax
 
 
-def has_change(axons_prev, axons_new):
-    for i in range(len(axons_prev)):
-        if axons_prev[i] != axons_new[i]:
-            return True
-    return False
+def has_change(prev_ax, new_ax):
+    boolean = False
+    for i in range(len(prev_ax)):
+        if prev_ax[i] != new_ax[i]:
+            boolean = True
+    return boolean
 
 
-def compare_two_vector(pr_vec, new_vec, eps):
+def compare_two_vector(prev_vector, new_vector, eps):
     dif = 0
-    for i in range(len(pr_vec)):
-        dif += math.pow(pr_vec[i] - new_vec[i], 2)
+    for i in range(len(prev_vector)):
+        dif += math.pow(prev_vector[i] - new_vector[i], 2)
     if math.pow(dif, 1 / 2) < eps:
         return False
     else:
         return True
 
 
-def init_hopfield(train, test, n_stand, n_col, n_row):
+def start_hopfield(train, test, cols, rows):
     vec_train = matrx_to_vec(train)
-    weights = np.zeros((n_col * n_row, n_col * n_row))
+    stands = int(len(vec_train)/len(matrx_to_vec(test)))
+    weights = np.zeros((cols * rows, cols * rows))
     for i in range(weights.shape[0]):
         for j in range(i, weights.shape[1]):
             if i != j:
-                for z in range(0, n_stand):
-                    weights[i][j] += vec_train[i + z * n_col * n_row] * vec_train[j + z * n_col * n_row]
+                for z in range(0, stands):
+                    weights[i][j] += vec_train[i + z * cols * rows] * vec_train[j + z * cols * rows]
                 weights[j][i] = weights[i][j]
     test = matrx_to_vec(test)
-    pr_ax = test
-    indicator = True
+    prev = test
+    infinity = True
     count = 0
-    while indicator:
-        n_ax = cal_of_neu_hopfield(pr_ax, weights)
-        indicator = has_change(pr_ax, n_ax)
-        pr_ax = n_ax
+    while infinity:
+        new = get_hopfield(prev, weights)
+        infinity = has_change(prev, new)
+        prev = new
         count += 1
         if count % 1000 == 0:
             print(f"Количество итераций: {count}")
         if count > 100000:
             break
-    pr_ax = vector_to_matrx(pr_ax, n_col)
-    matrx_to_PNG(pr_ax)
+    prev = vector_to_matrx(prev, cols)
+    matrx_to_PNG(prev)
 
 
-def init_hamming(train, test):
-    n_col = train.shape[1]
-    n_row = int(train.shape[0] / 4)
-    weights = np.zeros((n_col * n_row, 4))
+def start_hamming(train, test):
+    cols = train.shape[1]
+    rows = int(train.shape[0] / 4)
+    weights = np.zeros((cols * rows, 4))
     for i in range(weights.shape[0]):
         for j in range(weights.shape[1]):
-            weights[i][j] = train.at[(i // n_col) + j * n_row, i % n_col] / 2
+            weights[i][j] = train.at[(i // cols) + j * rows, i % cols] / 2
     test = matrx_to_vec(test)
-    pr_ax = cal_of_neu_hamming(test, weights, t=n_col * n_row / 2)
-    indicator = True
+    prev = get_hamming(test, weights, t=cols * rows / 2)
+    infinity = True
     count = 0
-    while indicator:
-        n_ax = cal_of_ax_hamming(pr_ax, eps=0.01)
-        indicator = compare_two_vector(pr_ax, n_ax, eps=0.1)
-        pr_ax = n_ax
+    while infinity:
+        new = get_new_hamming(prev, eps=0.01)
+        infinity = compare_two_vector(prev, new, eps=0.1)
+        prev = new
         count += 1
         if count % 1000 == 0:
             print(f"Количество итераций: {count}")
         if count > 10000:
             break
-    print(pr_ax)
+    print(prev)
--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import pandas as pd
 
-from func import init_hamming, init_hopfield
+from func import start_hamming, start_hopfield
 
-train_sample = pd.read_excel('examples.xlsx', usecols="B:I", names=[0, 1, 2, 3, 4, 5, 6, 7], nrows=32,
-                             engine='openpyxl')
-test_sample = pd.read_excel('examples.xlsx', usecols="B:I", names=[0, 1, 2, 3, 4, 5, 6, 7], skiprows=32,
-                            engine='openpyxl')
-n_col = train_sample.shape[1]
-n_row = int(train_sample.shape[0] / 4)
+train_s = pd.read_excel('traintest.xlsx', usecols="B:I", names=[0, 1, 2, 3, 4, 5, 6, 7], nrows=32,
+                        engine='openpyxl')
+test_s = pd.read_excel('traintest.xlsx', usecols="B:I", names=[0, 1, 2, 3, 4, 5, 6, 7], skiprows=32,
+                       engine='openpyxl')
+cols = train_s.shape[1]
+rows = int(train_s.shape[0] / 4)
 
-init_hopfield(train=train_sample, test=test_sample, n_stand=4, n_col=n_col, n_row=n_row)
-init_hamming(train_sample, test_sample)
+start_hopfield(train=train_s, test=test_s, cols=cols, rows=rows)
+start_hamming(train_s, test_s)
